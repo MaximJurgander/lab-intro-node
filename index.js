e7cc1db878b7a613a43186ffbcd600e9f3f7cc14
Maxim Jurgandere <maxim.jurgander@gmail.com>
2020-11-07 01:43:43 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] == undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  } 
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+     return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+     return Math.min(...this.items);
+    }
+  }
+  // Should work
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+     return this.items.reduce((sum, num) => sum + num, 0);
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((sum, num) => sum + num, 0) / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
